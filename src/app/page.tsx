5548d9c890b2c6dcf6b82825b4d10de2162c5bb1
JeanVittory <vittory.dev@gmail.com>
2025-03-22 16:41:13 -0500

    feat: :sparkles: Add new components into the main component tree and add some position styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { InputMessage } from '@/components/ui/inputMessage';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { UserLabel } from '@/components/ui/userLabel';
+import { ChatUserOptions } from '@/components/ui/chatUserOptions';
 
 export default function Home() {
   return (
     <Box>
+      <Flex
+        p=".8rem"
+        alignItems="center"
+        justifyContent="space-between"
+        bgColor="#0D1322"
+        borderBottom="1px solid rgba(255, 255, 255, 0.1)"
+      >
+        <UserLabel />
+        <ChatUserOptions />
+      </Flex>
       <Box
         position="fixed"
         bottom="0"
