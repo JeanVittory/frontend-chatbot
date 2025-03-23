5ce29c54fd262b4ddf743bfea95af93b6e893438
JeanVittory <vittory.dev@gmail.com>
2025-03-23 18:52:55 -0500

    feat: :truck: Change the name of the componente to Chat

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,37 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { InputMessage } from '@/components/ui/inputMessage';
+import { UserLabel } from '@/components/ui/userLabel';
+import { ChatUserOptions } from '@/components/ui/chatUserOptions';
+import { Message } from '@/components/ui/message';
+
+export default function Chat() {
+  return (
+    <Box>
+      <Flex
+        p=".8rem"
+        alignItems="center"
+        justifyContent="space-between"
+        bgColor="#0D1322"
+        borderBottom="1px solid rgba(255, 255, 255, 0.1)"
+      >
+        <UserLabel />
+        <ChatUserOptions />
+      </Flex>
+      <Flex flexDirection="column" m="auto" maxWidth="800px" px="1rem">
+        <Message isHuman={true} />
+        <Message isHuman={false} />
+      </Flex>
+      <Box
+        position="fixed"
+        bottom="0"
+        left="0"
+        width="100%"
+        padding="1rem"
+        bgColor="#0D1322"
+        borderTop="1px solid rgba(255, 255, 255, 0.1)"
+      >
+        <InputMessage />
+      </Box>
+    </Box>
+  );
+}
