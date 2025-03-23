f5847088d1f103ee0213283054f301622bbf08d9
JeanVittory <vittory.dev@gmail.com>
2025-03-23 18:46:56 -0500

    feat: :wrench: Add Providers component to use next auth SessionProvider in the client side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Provider } from '@/components/ui/provider';
+import { Providers } from './providers';
 import './globals.css';
 
 const geistSans = Geist({
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        <Providers>
+          <Provider>{children}</Provider>
+        </Providers>
       </body>
     </html>
   );
