b340c51be20d12d0365a8e74a49f7dfa859f5baa
JeanVittory <vittory.dev@gmail.com>
2025-03-27 21:51:24 -0500

    feat: :sparkles: Add logout button to close session

--- src/components/ui/chatUserOptions.tsx
+++ src/components/ui/chatUserOptions.tsx
@@ -1,14 +1,36 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
 import { IoCallOutline } from 'react-icons/io5';
 import { PiVideoCamera } from 'react-icons/pi';
 import { HiDotsVertical } from 'react-icons/hi';
-import { Flex } from '@chakra-ui/react';
+import { RiLogoutBoxRLine } from 'react-icons/ri';
+import { Flex, Box } from '@chakra-ui/react';
 
 export const ChatUserOptions = () => {
   return (
     <Flex gapX="2rem">
-      <IoCallOutline size="1.2rem" color="#F3F4F6" />
-      <PiVideoCamera size="1.2rem" color="#F3F4F6" />
-      <HiDotsVertical size="1.2rem" color="#F3F4F6" />
+      <Box>
+        <IoCallOutline size="1.2rem" color="#F3F4F6" />
+      </Box>
+      <Box>
+        <PiVideoCamera size="1.2rem" color="#F3F4F6" />
+      </Box>
+      <Box>
+        <HiDotsVertical size="1.2rem" color="#F3F4F6" />
+      </Box>
+      <Box
+        cursor={{
+          base: 'auto',
+          md: 'pointer',
+        }}
+      >
+        <RiLogoutBoxRLine
+          size="1.2rem"
+          color="#F3F4F6"
+          onClick={() => signOut({ callbackUrl: '/' })}
+        />
+      </Box>
     </Flex>
   );
 };
