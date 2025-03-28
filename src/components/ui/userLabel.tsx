b8444a3bd2aa25246f724c7c857dad3a581b2e1d
JeanVittory <vittory.dev@gmail.com>
2025-03-28 18:45:27 -0500

    feat: :sparkles: Add user information within the UserLabel component

--- src/components/ui/userLabel.tsx
+++ src/components/ui/userLabel.tsx
@@ -1,15 +1,18 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { Avatar, Flex, Text } from '@chakra-ui/react';
+import { getServerSession } from 'next-auth';
 
-export const UserLabel = () => {
+export const UserLabel = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <Flex gapX=".5rem">
       <Avatar.Root size="lg">
         <Avatar.Fallback name="You" />
-        <Avatar.Image />
+        <Avatar.Image src={session.user.image} />
       </Avatar.Root>
       <Flex flexDirection="column" justifyContent="center" gapY=".1rem">
         <Text as="h3" color="#F3F4F6" fontSize="1rem" lineHeight="1rem">
-          Jean Carlo
+          {session.user.name}
         </Text>
         <Text as="p" color="#9CA3AF" fontSize=".8rem" lineHeight="1rem">
           Online
