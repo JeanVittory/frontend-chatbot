12aa0be59c278055268cb6326f3cb379de7f5891
JeanVittory <vittory.dev@gmail.com>
2025-04-15 22:21:37 -0500

    feat: :sparkles: Add a Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Flex } from '@chakra-ui/react';
+import { UserLabel } from './UserLabel';
+import { ChatUserOptions } from './ChatUserOptions';
+
+export const Header = () => {
+  return (
+    <Flex
+      position="fixed"
+      width="100%"
+      p=".8rem"
+      alignItems="center"
+      justifyContent="space-between"
+      bgColor="#0D1322"
+      borderBottom="1px solid rgba(255, 255, 255, 0.1)"
+    >
+      <UserLabel />
+      <ChatUserOptions />
+    </Flex>
+  );
+};
