7b13c858e7009d0225d5aac69397224ac8ff6f8c
JeanVittory <vittory.dev@gmail.com>
2025-04-15 22:20:13 -0500

    feat: :recycle: Add a better code experience creating components and removing some dead code in chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,48 +1,12 @@
-import { Box, Flex } from '@chakra-ui/react';
-import { InputMessage } from '@/components/ui/inputMessage';
-import { UserLabel } from '@/components/ui/userLabel';
-import { ChatUserOptions } from '@/components/ui/chatUserOptions';
-import { Message } from '@/components/ui/message';
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '../api/auth/[...nextauth]/route';
-import { getChatHistory } from '../services/chatHistory';
+import { Box } from '@chakra-ui/react';
+import { InputMessage } from '@/app/components/InputMessage';
+// import { WebsocketProvider } from '../providers/WebsocketProvider';
+import { Messages } from '../components/Messages';
 
 export default async function Chat() {
-  //const session = await getServerSession(authOptions);
-  const chatHistory = await getChatHistory('user_session_new');
-
   return (
     <Box>
-      <Flex
-        position="fixed"
-        width="100%"
-        p=".8rem"
-        alignItems="center"
-        justifyContent="space-between"
-        bgColor="#0D1322"
-        borderBottom="1px solid rgba(255, 255, 255, 0.1)"
-      >
-        <UserLabel />
-        <ChatUserOptions />
-      </Flex>
-      <Flex
-        flexDirection="column"
-        m="auto"
-        maxWidth="800px"
-        px="1rem"
-        pb="6rem"
-      >
-        {chatHistory.message.map((message) => {
-          return (
-            <Message
-              createdAt={message.createdAt}
-              key={message.id}
-              isHuman={message.type === 'human'}
-              message={message.content}
-            />
-          );
-        })}
-      </Flex>
+      <Messages />
       <Box
         position="fixed"
         bottom="0"
