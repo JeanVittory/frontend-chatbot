ea93e43174ea20829bf0a184585e1b188ec3c965
JeanVittory <vittory.dev@gmail.com>
2025-04-16 17:08:57 -0500

    feat: :sparkles: Add automatic scroll into Messages list to show the most recents messages

--- src/app/components/Messages.tsx
+++ src/app/components/Messages.tsx
@@ -2,11 +2,23 @@
 
 import { Flex } from '@chakra-ui/react';
 import { Message } from './Message';
-
+import { useEffect, useRef } from 'react';
 import { useWebSocketStore } from '../store/websocketStore';
+import { Spinner } from '@/components/ui/spinner';
 
 export const Messages = () => {
   const { messages } = useWebSocketStore();
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  if (!messages.length) return <Spinner />;
   return (
     <Flex flexDirection="column" m="auto" maxWidth="800px" px="1rem" py="6rem">
       {messages &&
@@ -20,6 +32,7 @@ export const Messages = () => {
             />
           );
         })}
+      <div ref={messagesEndRef} />
     </Flex>
   );
 };
