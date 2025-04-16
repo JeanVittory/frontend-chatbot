f0b460c2f9278c15181167f545564439317c8b70
JeanVittory <vittory.dev@gmail.com>
2025-04-16 17:12:25 -0500

    feat: :sparkles: improve the socket connection, the user validation and the add messages process in the messages state

--- src/app/store/websocketStore.ts
+++ src/app/store/websocketStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import { getChatHistory } from '../services/chatHistory';
 import { Message } from '../types/chatHistory';
+import { v4 as uuidv4 } from 'uuid';
 
 type WebSocketState = {
   socket: WebSocket | null;
@@ -8,24 +9,41 @@ type WebSocketState = {
   connect: (url: string, userId: string) => void;
   sendMessage: (message: string) => void;
   error: boolean;
+  isConnected: boolean;
 };
 
 export const useWebSocketStore = create<WebSocketState>((set, get) => ({
   socket: null,
   messages: [],
   error: false,
+  isConnected: false,
   connect: (url: string, userId: string) => {
     const socket = new WebSocket(url);
     socket.onopen = async () => {
       try {
+        socket.send(userId);
         const chatHistory = await getChatHistory(userId);
-        set({ messages: [...get().messages, ...chatHistory.message] });
+        set({
+          messages: [...get().messages, ...chatHistory.message],
+          isConnected: true,
+        });
       } catch (error) {
         console.error(error);
+        set({ error: true });
       }
     };
     socket.onmessage = (event) => {
-      set({ messages: [...get().messages, event.data] });
+      set({
+        messages: [
+          ...get().messages,
+          {
+            content: event.data,
+            id: uuidv4(),
+            createdAt: new Date(),
+            type: 'ai',
+          },
+        ],
+      });
     };
 
     socket.onerror = () => {
@@ -34,6 +52,20 @@ export const useWebSocketStore = create<WebSocketState>((set, get) => ({
     set({ socket });
   },
   sendMessage: (message: string) => {
-    get().socket?.send(message);
+    const { isConnected, socket } = get();
+    set({
+      messages: [
+        ...get().messages,
+        {
+          content: message,
+          id: uuidv4(),
+          createdAt: new Date(),
+          type: 'human',
+        },
+      ],
+    });
+    if (socket && isConnected) {
+      socket.send(message);
+    }
   },
 }));
